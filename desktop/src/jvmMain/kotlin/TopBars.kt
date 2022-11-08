d89d87e4a8952ac288108e21a1e3245d8323e4d7
Jacob Rein <jakerein@gmail.com>
2022-11-08 12:28:34 -0500

    Coloring icons

--- desktop/src/jvmMain/kotlin/TopBars.kt
+++ desktop/src/jvmMain/kotlin/TopBars.kt
@@ -1,7 +1,10 @@
+import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.combinedClickable
+import androidx.compose.foundation.hoverable
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsHoveredAsState
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.window.WindowDraggableArea
@@ -10,9 +13,11 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.*
@@ -97,21 +102,41 @@ fun ApplicationScope.LinuxTopBar(state: WindowState, onExit: () -> Unit, windowT
             modifier = Modifier.align(Alignment.CenterEnd),
             horizontalArrangement = Arrangement.Start
         ) {
-            IconButton(onClick = onExit) {
+            val hoverInteraction = remember { MutableInteractionSource() }
+            val isHovering by hoverInteraction.collectIsHoveredAsState()
+            IconButton(
+                onClick = onExit,
+                modifier = Modifier.hoverable(hoverInteraction)
+            ) {
                 Icon(
                     Icons.Default.Close,
-                    null
+                    null,
+                    tint = animateColorAsState(if (isHovering) Color.Red else LocalContentColor.current).value
                 )
             }
-            IconButton(onClick = {
-                state.isMinimized = !state.isMinimized
-            }) { Icon(Icons.Default.Minimize, null) }
+            IconButton(
+                onClick = { state.isMinimized = !state.isMinimized },
+                modifier = Modifier.hoverable(hoverInteraction)
+            ) {
+                Icon(
+                    Icons.Default.Minimize,
+                    null,
+                    tint = animateColorAsState(if (isHovering) Color.Yellow else LocalContentColor.current).value
+                )
+            }
             IconButton(
                 onClick = {
                     state.placement = if (state.placement != WindowPlacement.Maximized) WindowPlacement.Maximized
                     else WindowPlacement.Floating
-                }
-            ) { Icon(Icons.Default.Maximize, null) }
+                },
+                modifier = Modifier.hoverable(hoverInteraction)
+            ) {
+                Icon(
+                    Icons.Default.Maximize,
+                    null,
+                    tint = animateColorAsState(if (isHovering) Color.Green else LocalContentColor.current).value
+                )
+            }
         }
 
         Text(
@@ -128,21 +153,41 @@ fun ApplicationScope.WindowsTopBar(state: WindowState, onExit: () -> Unit, windo
             modifier = Modifier.align(Alignment.CenterEnd),
             horizontalArrangement = Arrangement.Start
         ) {
-            IconButton(onClick = onExit) {
+            val hoverInteraction = remember { MutableInteractionSource() }
+            val isHovering by hoverInteraction.collectIsHoveredAsState()
+            IconButton(
+                onClick = onExit,
+                modifier = Modifier.hoverable(hoverInteraction)
+            ) {
                 Icon(
                     Icons.Default.Close,
-                    null
+                    null,
+                    tint = animateColorAsState(if (isHovering) Color.Red else LocalContentColor.current).value
                 )
             }
-            IconButton(onClick = {
-                state.isMinimized = !state.isMinimized
-            }) { Icon(Icons.Default.Minimize, null) }
+            IconButton(
+                onClick = { state.isMinimized = !state.isMinimized },
+                modifier = Modifier.hoverable(hoverInteraction)
+            ) {
+                Icon(
+                    Icons.Default.Minimize,
+                    null,
+                    tint = animateColorAsState(if (isHovering) Color.Yellow else LocalContentColor.current).value
+                )
+            }
             IconButton(
                 onClick = {
                     state.placement = if (state.placement != WindowPlacement.Maximized) WindowPlacement.Maximized
                     else WindowPlacement.Floating
-                }
-            ) { Icon(Icons.Default.Maximize, null) }
+                },
+                modifier = Modifier.hoverable(hoverInteraction)
+            ) {
+                Icon(
+                    Icons.Default.Maximize,
+                    null,
+                    tint = animateColorAsState(if (isHovering) Color.Green else LocalContentColor.current).value
+                )
+            }
         }
 
         Text(
@@ -159,24 +204,39 @@ fun ApplicationScope.MacOsTopBar(state: WindowState, onExit: () -> Unit, windowT
             modifier = Modifier.align(Alignment.CenterStart),
             horizontalArrangement = Arrangement.Start
         ) {
-            IconButton(onClick = onExit) {
+            val hoverInteraction = remember { MutableInteractionSource() }
+            val isHovering by hoverInteraction.collectIsHoveredAsState()
+            IconButton(
+                onClick = onExit,
+                modifier = Modifier.hoverable(hoverInteraction)
+            ) {
                 Icon(
                     Icons.Default.Close,
-                    null
+                    null,
+                    tint = animateColorAsState(if (isHovering) Color.Red else LocalContentColor.current).value
                 )
             }
-            IconButton(onClick = {
-                state.isMinimized = !state.isMinimized
-            }) { Icon(Icons.Default.Minimize, null) }
+            IconButton(
+                onClick = { state.isMinimized = !state.isMinimized },
+                modifier = Modifier.hoverable(hoverInteraction)
+            ) {
+                Icon(
+                    Icons.Default.Minimize,
+                    null,
+                    tint = animateColorAsState(if (isHovering) Color.Yellow else LocalContentColor.current).value
+                )
+            }
             IconButton(
                 onClick = {
                     state.placement = if (state.placement != WindowPlacement.Fullscreen) WindowPlacement.Fullscreen
                     else WindowPlacement.Floating
-                }
+                },
+                modifier = Modifier.hoverable(hoverInteraction)
             ) {
                 Icon(
                     if (state.placement != WindowPlacement.Fullscreen) Icons.Default.Fullscreen else Icons.Default.FullscreenExit,
-                    null
+                    null,
+                    tint = animateColorAsState(if (isHovering) Color.Green else LocalContentColor.current).value
                 )
             }
         }
