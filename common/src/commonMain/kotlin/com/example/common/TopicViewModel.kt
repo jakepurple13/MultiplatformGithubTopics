42f97567c86330e790735200d7af37fa8efacadb
Jacob Rein <jakerein@gmail.com>
2022-11-09 09:55:59 -0500

    Adding cache

--- common/src/commonMain/kotlin/com/example/common/TopicViewModel.kt
+++ common/src/commonMain/kotlin/com/example/common/TopicViewModel.kt
@@ -67,13 +67,23 @@ class BaseRepoViewModel(
     override var error by mutableStateOf(false)
 
     override suspend fun load() {
-        Network.getReadMe(item.fullName).fold(
-            onSuccess = { repoContent = it },
-            onFailure = {
-                it.printStackTrace()
-                error = true
-            }
-        )
+        val cached = Cached.cache[item.htmlUrl]
+        if (cached != null) {
+            println("Loading from cache")
+            repoContent = ReadMeResponse.Success(cached.repoContent)
+        } else {
+            println("Loading from url")
+            Network.getReadMe(item.fullName).fold(
+                onSuccess = {
+                    repoContent = it
+                    if (it is ReadMeResponse.Success) Cached.cache[item.htmlUrl] = CachedTopic(item, it.content)
+                },
+                onFailure = {
+                    it.printStackTrace()
+                    error = true
+                }
+            )
+        }
     }
 }
 
